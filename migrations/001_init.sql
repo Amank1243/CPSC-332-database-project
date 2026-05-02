9af08d1c68bf56374b1b8097a2579f1404e20662
Amank1243 <148727967+Amank1243@users.noreply.github.com>
2026-05-01 21:19:27 -0700

    Added query for creating account table

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -32,3 +32,15 @@ create table transactionType (
   typeName transaction_types
 );
 
+create table account (
+  accountId int primary key,
+  customerId int,
+  accountType varchar(50),
+  balance decimal,
+  dateopened date,
+  status varchar(20),
+  
+-- this is saying customerId in the account table is a foreign key, and references the customerId in the customer table
+-- without this, an employee can potentially input data where the customer id in the customer table does not exist when inputting data into the account table, meaning the customer does not exist rendering the data pointless and false
+  foreign key (customerId) references Customer(customerid)
+)
