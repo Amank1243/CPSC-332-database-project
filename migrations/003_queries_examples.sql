74db8bd9abd59853d42a3ab6e904dd56f3ca0d82
Amank1243 <148727967+Amank1243@users.noreply.github.com>
2026-05-04 20:53:06 -0700

    Added update query to normalize transaction amount

--- migrations/003_queries_examples.sql
+++ migrations/003_queries_examples.sql
@@ -10,11 +10,11 @@
 
 -- Each query must include a short comment describing what it does 
 
--- Triggers
+-------------------------------------- Triggers -----------------------------------------------
 
--- Views
+-------------------------------------- Views -----------------------------------------------
 
--- Reads
+-------------------------------------- Reads -----------------------------------------------
 
 -- Calculates the balance of accounts. Shows the full name, customerid, and balance
 -- Useful if an employee suspects that the database is incorrectly calculating the balance of customers in account
@@ -22,9 +22,14 @@ select c.fullname, a.customerid ,SUM(bt.amount) as balance
 from customer c 
 inner join account a on a.customerid = c.customerid
 inner join banktransaction bt on bt.accountid = a.accountid
-group by c.fullname, a.customerid
+group by c.fullname, a.customerid;
 
--- Updates
-
-
+-------------------------------------- Updates -----------------------------------------------
+-- Update bank transaction amount to have a swapped sign depending on whether or not money is being taken away or given to the account
+update banktransaction bt
+set amount = case
+when bt.typeid in (1, 6, 7) and (bt.amount < 0) then bt.amount * -1 
+when bt.typeid in (2,3,4,5) and (bt.amount > 0) then bt.amount * -1
+else bt.amount
+end; 
 -- Deletes
