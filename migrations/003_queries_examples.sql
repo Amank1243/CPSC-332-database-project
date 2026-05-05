e96cf977f200b78a027b0c77bfe8533e14e4dcab
Amank1243 <148727967+Amank1243@users.noreply.github.com>
2026-05-04 18:15:16 -0700

    Added basic template via comments for queries file

---
+++ migrations/003_queries_examples.sql
@@ -0,0 +1,21 @@
+-- Minimum requirement:
+
+--     Include TRIGGER: at least 1 trigger
+--     Include VIEW: at least 1 view
+--     Include READ (SELECT): at least 6 queries including JOINs
+--     Include UPDATE: at least 6 queries
+--     Include DELETE: at least 6 queries
+
+-- Total minimum: 19 queries.
+
+-- Each query must include a short comment describing what it does 
+
+-- Triggers
+
+-- Views
+
+-- Reads
+
+-- Updates
+
+-- Deletes
